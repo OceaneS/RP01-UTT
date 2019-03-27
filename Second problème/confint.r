684f0ec9d1e0c1d0363ed0c96f7b5add4ca08083
CeyFun <baptis59@gmail.com>
2019-03-27 14:07:42 +0100

    Ajout d'un graph

--- Second problème/confint.r
+++ Second problème/confint.r
@@ -13,3 +13,11 @@ conf.int <- u*sqrt(1/6*(1-1/6)/n)
 png("Intervalle de confiance en fonction du niveau de confiance.png")
 plot(conf.level, conf.int, type="l", main="Intervalle de confiance en fonction du niveau de confiance", xlab="Niveau de confiance", ylab="Longueur de l'intervalle")
 invisible(dev.off())
+n <- 100
+conf.level <- 0.95
+u <- qnorm(1 - (1-conf.level)/2)
+p <- 0.01*(1:99)
+conf.int <- u*sqrt(p*(1-p)/n)
+png("Intervalle de confiance en fonction de la probabilite.png")
+plot(p, conf.int, type="l", main="Intervalle de confiance en fonction de la probabilite", xlab="Probabilite", ylab="Longueur de l'intervalle")
+invisible(dev.off())
